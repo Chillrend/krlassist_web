3dd3f92b67909b18d1a1000ea5496e816a039baf
Farhan Hanif <madjavacoder@gmail.com>
2019-06-17 20:53:35 +0700

    Fixed weird appengine bug, now the app is deployed correctly

--- src/main/java/krlweb/filters/AuthFilter.java
+++ src/main/java/krlweb/filters/AuthFilter.java
@@ -3,6 +3,7 @@ package krlweb.filters;
 import krlweb.utils.SessionObject;
 import krlweb.utils.SessionUtils;
 
+import javax.faces.application.ResourceHandler;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -22,30 +23,38 @@ public class AuthFilter implements Filter {
 
     }
 
+    private static final String AJAX_REDIRECT_XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            + "<partial-response><redirect url=\"%s\"></redirect></partial-response>";
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException,ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpSession session = request.getSession(false);
+        String loginURL = request.getContextPath() + "/faces/login.xhtml";
 
-        try{
-            HttpServletRequest reqt = (HttpServletRequest) req;
-            HttpServletResponse resp = (HttpServletResponse) res;
+        boolean loggedIn = (session != null) && (session.getAttribute("session") != null);
+        boolean loginRequest = request.getRequestURI().equals(loginURL);
+        boolean resourceRequest = request.getRequestURI().contains("javax.faces.resource");
+        boolean ajaxRequest = "partial/ajax".equals(request.getHeader("Faces-Request"));
 
-            HttpSession session = ((HttpServletRequest) req).getSession(true);
-            String request_uri = reqt.getRequestURI();
-
-            if(request_uri.contains("javax.faces.resource") || session.getAttribute("session") != null){
-                chain.doFilter(req,res);
-            }else{
-                reqt.getRequestDispatcher("/faces/login.xhtml").forward(req,res);
-//                resp.sendRedirect(reqt.getContextPath() + "/faces/login.xhtml");
-//                reqt.getSession().getServletContext().getRequestDispatcher("/faces/login.xhtml").forward(req,res);
+        if (loggedIn || loginRequest || resourceRequest) {
+            if (!resourceRequest) { // Prevent browser from caching restricted resources. See also https://stackoverflow.com/q/4194207/157882
+                response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+                response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+                response.setDateHeader("Expires", 0); // Proxies.
             }
-        }catch(Exception e){
 
-            e.printStackTrace();
-            System.out.print(e.getMessage());
-
+            chain.doFilter(request, response); // So, just continue request.
         }
-
+        else if (ajaxRequest) {
+            response.setContentType("text/xml");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().printf(AJAX_REDIRECT_XML, loginURL); // So, return special XML response instructing JSF ajax to send a redirect.
+        }
+        else {
+            response.sendRedirect(loginURL); // So, just perform standard synchronous redirect.
+        }
     }
 
     @Override
--- src/main/webapp/resources/scripts/routemap.js
+++ src/main/webapp/resources/scripts/routemap.js
@@ -2,7 +2,147 @@ var map;
 
 function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {lat: -6.2099, lng: 106.8502},
+        zoom: 15
     });
+}
+
+var line_ref = db.collection('line');
+var line_global = {};
+
+var stasiun_ref = db.collection('stasiun');
+var stasiun_global ={};
+
+var isStasiunLoaded = false;
+
+$(document).ready(function () {
+    line_ref.get().then(snapshot => {
+        return snapshot.docs.map(doc => doc)
+    }).then(lines => {
+        lines.map(line => {
+            let real_line = line.data();
+            line_global[line.id] = JSON.parse(JSON.stringify(real_line))
+        });
+
+        return line_global;
+    }).then(final_result => {
+        for (let key in final_result){
+            if(final_result.hasOwnProperty(key)){
+                $('#sf-form').append('<option value="'+ key +'">Rute '+ key +'</option>');
+            }
+        }
+    }).catch(error => console.error(error.stack));
+
+
+    stasiun_ref.get().then(snapshot => {
+        return snapshot.docs.map(doc => doc)
+    }).then(stasiuns => {
+        let st_data = {};
+        stasiuns.map(stasiun => {
+            let real_stasiun = stasiun.data();
+            stasiun_global[stasiun.id] = JSON.parse(JSON.stringify(real_stasiun));
+        });
+
+        isStasiunLoaded = true;
+    }).catch(error => console.error(error.stack));
+
+});
+
+$('select').on('change', function () {
+    let line = getLineFromLineName(this.value);
+
+    if(isStasiunLoaded){
+        renderMap(line);
+    }else{
+        Swal.fire('Mohon Tunggu', 'Daftar Stasiun sedang dimuat, mohon tunggu', 'warning');
+    }
+
+});
+
+var routePath;
+var markers = [];
+
+function addMarker(location){
+    let marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+
+    markers.push(marker);
+}
+
+function setMapOnAll(map){
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+function clearMarker() {
+    setMapOnAll(null);
+}
+
+function showMarker(){
+    setMapOnAll(map);
+}
+
+function deleteMarker() {
+    clearMarker();
+    markers = [];
+}
+
+function renderMap(line){
+
+    deleteMarker();
+
+    if(typeof routePath !== 'undefined'){
+        routePath.setPath([]);
+        routePath.setMap(null);
+    }
+
+    let stasiun_served = line.stasiun_served;
+    let geodesicCoordinates = [];
+    for(let i=0; i<stasiun_served.length; i++){
+        for(let key in stasiun_global){
+            if(stasiun_global.hasOwnProperty(key)){
+                if(key === stasiun_served[i]){
+                    let lat_pos = stasiun_global[key].latitude;
+                    let lng_pos = stasiun_global[key].longitude;
+
+                    let pos = {lat: lat_pos, lng: lng_pos};
+
+                    let marker = new google.maps.Marker({
+                        position: pos,
+                        map: map,
+                        title: 'Stasiun ' + key
+                    });
+
+                    markers.push(marker)
+
+                    geodesicCoordinates.push(pos);
+                }
+            }
+        }
+    }
+
+    showMarker();
+
+    routePath = new google.maps.Polyline({
+        path: geodesicCoordinates,
+        geodesic: true,
+        strokeColor: '#FF000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+
+    routePath.setMap(map);
+}
+
+function getLineFromLineName(line_name){
+    for(let key in line_global){
+        if(line_global.hasOwnProperty(key)){
+            if(key === line_name){
+                return JSON.parse(JSON.stringify(line_global[key]))
+            }
+        }
+    }
 }
